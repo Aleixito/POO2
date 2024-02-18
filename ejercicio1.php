c905664994fafaff04e9bb9344fe378e1e8b0e7f
Aleixito <aleixtomu@gmail.com>
2024-02-19 00:02:03 +0100

    1st

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,5 +1,20 @@
 
 <?php 
 
+interface SonidoAnimal {
+    public function getSound(): string;
+}
+
+class Ladrido implements SonidoAnimal {
+    public function getSound(): string {
+        return "Bup, bup!";
+    }
+}
+
+class Maullido implements SonidoAnimal {
+    public function getSound(): string {
+        return "Mi!";
+    }
+}
 
 ?>
