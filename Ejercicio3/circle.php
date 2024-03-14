c432c8853b4c7b80cde8b81ac70f69d0a634ee04
Aleixito <aleixtomu@gmail.com>
2024-03-14 13:00:22 +0100

    3rd exercise

---
+++ Ejercicio3/circle.php
@@ -0,0 +1,17 @@
+
+<?php
+class Circle extends Shape
+{
+    protected $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function area()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}
+?>
